4265b1da63967a3d920465f8a0485929a8efe0de
repleyva <rusvelpassos@gmail.com>
2021-10-14 14:00:25 -0500

    Modificando imagenes del portafolio

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,11 +3,12 @@ export const data = [
   {
     id: 1,
     title: "Harto E' Buti App",
-    img: "https://hartoebuti.herokuapp.com/assets/capture-harto-e-buti.png",
+    /* img: "https://hartoebuti.herokuapp.com/assets/capture-harto-e-buti.png", */
+    img: "assets/harto-e-buti.png",
   },
   {
     id: 2,
     title: "Netflix Clone App",
-    img: "https://netflix-clone-repleyva.netlify.app/clone.png",
+    img: "assets/netflix-clone.png",
   },
 ];
