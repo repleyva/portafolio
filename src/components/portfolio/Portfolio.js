16031a382e7f80a2cbb8cb903bebcefdb6ac60cb
repleyva <rusvelpassos@gmail.com>
2021-10-14 12:01:24 -0500

    Portafolio

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,38 +1,41 @@
+import "./portfolio.css";
+import { data } from "../../utils/data.js";
+import LinkIcon from "@mui/icons-material/Link";
+
 const Portfolio = () => {
+  const handleClick = (el) => {
+    if (el.id === 1) {
+      window.open("https://hartoebuti.herokuapp.com/");
+      console.log(el);
+    }
+    if (el.id === 2) {
+      window.open("https://netflix-clone-repleyva.netlify.app/");
+      console.log(el);
+    }
+  };
   return (
-    <section id="portfolio">
+    <section id="portfolio" className="port-container">
       <h2>Portfolio</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, provident
-        impedit! Sapiente animi voluptatem voluptatibus dicta ducimus fugit ipsa
-        ratione nesciunt quaerat nemo voluptatum quia, doloribus corporis
-        deserunt eligendi exercitationem! Lorem ipsum, dolor sit amet
-        consectetur adipisicing elit. Nobis officia quia eum, iure quos
-        voluptatem rerum veniam exercitationem totam similique recusandae
-        doloribus impedit! Nam molestiae suscipit quisquam veritatis alias
-        aspernatur? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Eius, provident impedit! Sapiente animi voluptatem voluptatibus dicta
-        ducimus fugit ipsa ratione nesciunt quaerat nemo voluptatum quia,
-        doloribus corporis deserunt eligendi exercitationem! Lorem ipsum, dolor
-        sit amet consectetur adipisicing elit. Nobis officia quia eum, iure quos
-        voluptatem rerum veniam exercitationem totam similique recusandae
-        doloribus impedit! Nam molestiae suscipit quisquam veritatis alias
-        aspernatur?Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Eius, provident impedit! Sapiente animi voluptatem voluptatibus dicta
-        ducimus fugit ipsa ratione nesciunt quaerat nemo voluptatum quia,
-        doloribus corporis deserunt eligendi exercitationem! Lorem ipsum, dolor
-        sit amet consectetur adipisicing elit. Nobis officia quia eum, iure quos
-        voluptatem rerum veniam exercitationem totam similique recusandae
-        doloribus impedit! Nam molestiae suscipit quisquam veritatis alias
-        aspernatur? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Eius, provident impedit! Sapiente animi voluptatem voluptatibus dicta
-        ducimus fugit ipsa ratione nesciunt quaerat nemo voluptatum quia,
-        doloribus corporis deserunt eligendi exercitationem! Lorem ipsum, dolor
-        sit amet consectetur adipisicing elit. Nobis officia quia eum, iure quos
-        voluptatem rerum veniam exercitationem totam similique recusandae
-        doloribus impedit! Nam molestiae suscipit quisquam veritatis alias
-        aspernatur?
-      </p>
+      <div className="gallery-container">
+        {data.map((el, key) => (
+          <figure
+            key={key}
+            className="gallery__item card"
+            onClick={() => handleClick(el)}
+          >
+            <img src={el.img} alt={el.title} className="gallery__img" />
+            <figcaption className="port-overlay">
+              <div className="gallery__title-container">
+                <h3 className="gallery__title">{el.title}</h3>
+                <p>Sitio Responsivo</p>
+              </div>
+              <span>
+                <LinkIcon className="img" />
+              </span>
+            </figcaption>
+          </figure>
+        ))}
+      </div>
     </section>
   );
 };
---
+++ src/utils/data.js
@@ -0,0 +1,18 @@
+
+export const data = [
+  {
+    id: 1,
+    title: "Harto E' Buti App",
+    img: "https://hartoebuti.herokuapp.com/assets/capture-harto-e-buti.png",
+  },
+  {
+    id: 2,
+    title: "Netflix Clone App",
+    img: "https://netflix-clone-repleyva.netlify.app/clone.png",
+  },
+  {
+    id: 3,
+    title: "Netflix Clone App",
+    img: "https://netflix-clone-repleyva.netlify.app/clone.png",
+  },
+];
