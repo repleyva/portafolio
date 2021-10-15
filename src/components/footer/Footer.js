91d0ceb0048432e7a5ec0189f31ac150727c2ec5
repleyva <rusvelpassos@gmail.com>
2021-10-15 00:26:38 -0500

    footer terminado

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,41 @@
+import "./footer.css";
 const Footer = () => {
   return (
-    <div>
-      <h2 style={{margin: "0"}}>footer</h2>
-    </div>
+    <footer className="footerWrapper">
+      <div className="footer-icons">
+        <a
+          href="https://github.com/repleyva"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="assets/github.png" alt="github" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/rusvel-enrique-pasos-leyva-969b9918b/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="assets/linkedin.png" alt="linkedin" />
+        </a>
+        <a
+          href="https://twitter.com/repleyva"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="assets/twitter.png" alt="twitter" />
+        </a>
+        <a
+          href="https://wa.me/+573158160633/?text=Hola Rusvel, ¿Cómo estás?, %0AMe comunico contigo desde tu portafolio web. Me gustaría solicitar tus servicios como desarrollador."
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="assets/whatsapp.png" alt="whatsapp" />
+        </a>
+      </div>
+      <div className="footer-message">
+        <p>Rusvel Pasos - Todos los derechos reservados 2021</p>
+      </div>
+    </footer>
   );
 };
 
