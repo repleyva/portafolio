32b7a307f9d3e01bb8eb924f6144e2653d34746d
repleyva <rusvelpassos@gmail.com>
2021-10-14 20:50:15 -0500

    modificando los titulos

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,12 +1,12 @@
-import "./contact.css"
+import "./contact.css";
 const Contact = () => {
-	return (
+  return (
     <section id="contact" className="contact">
       <div className="contact-title">
         <h2>Contacto</h2>
       </div>
     </section>
   );
-}
+};
 
-export default Contact
+export default Contact;
