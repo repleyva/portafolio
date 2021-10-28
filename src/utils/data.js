01dabe626aa4137932975b9aa14c12d3a0510a22
repleyva <rusvelpassos@gmail.com>
2021-10-28 03:05:19 -0500

    adicionando new weather app

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,26 +1,32 @@
 export const data = [
-{
+  {
+    title: "Weather and News App",
+    img: "assets/weather-news-app.png",
+    link: "https://news-weather-app.netlify.app/",
+    desc: "Sitio Responsivo/React/CSS/Material UI",
+  },
+  {
     title: "Dashboard Theme",
     img: "assets/dashboard-theme.png",
     link: "https://dashboard-theme.netlify.app/",
-    desc: "React/Redux/Apexcharts",
+    desc: "React/CSS/Redux/Apexcharts",
   },
   {
     title: "Hotel Reservation App",
     img: "assets/reservation-hotel-app.png",
     link: "https://reservation-hotel-app.netlify.app/",
-    desc: "Sitio Responsivo/React/Redux/Material UI",
+    desc: "Sitio Responsivo/React/CSS/Redux/Material UI",
   },
   {
     title: "Netflix Clone App",
     img: "assets/netflix-clone.png",
     link: "https://netflix-clone-repleyva.netlify.app/",
-    desc: "React/Redux/Material UI",
+    desc: "React/CSS/Redux/Material UI",
   },
   {
     title: "Harto E' Buti App",
     img: "assets/harto-e-buti.png",
     link: "https://hartoebuti.herokuapp.com",
-    desc: "Sitio Responsivo/React/Material UI",
+    desc: "Sitio Responsivo/React/CSS/Material UI",
   },
 ];
